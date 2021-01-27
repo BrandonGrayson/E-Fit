5224f333740155d332ca60516890654a77b1e021
Brandon <brandonbgrayson@gmail.com>
2021-01-27 03:17:19 -0500

    can render html routes pages

---
+++ routes/htmlroutes.js
@@ -0,0 +1,17 @@
+// require path for routes
+var path = require('path');
+
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    app.get("/exercise", function (req, res) {
+        console.log("exercise")
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+
+    app.get("/stats", function (req,res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"))
+    });
+}
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
+
 const app = express();
 
 app.use(logger("dev"));
@@ -14,10 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/testdb", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", { useNewUrlParser: true })
 
 
 const PORT = process.env.PORT || 3000;
+
+// require routes
+require("./routes/htmlroutes")(app)
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);

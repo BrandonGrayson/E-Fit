be2a8cb5e531a9a7813540723fda32e56a4d0a1d
Brandon <brandonbgrayson@gmail.com>
2021-01-26 19:15:00 -0500

    established connection with server

---
+++ server.js
@@ -0,0 +1,24 @@
+// set up server connection
+
+// require modules
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/testdb", { useNewUrlParser: true })
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });

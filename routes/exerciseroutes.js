772f41a471d449fbc0188aba165d6f0c10d45f9f
Brandon <brandonbgrayson@gmail.com>
2021-01-27 21:36:58 -0500

    have the params: id in console

--- routes/exerciseroutes.js
+++ routes/exerciseroutes.js
@@ -19,11 +19,8 @@ module.exports = function (app) {
     app.put("/api/workouts/:id", function (req, res) {
         // console.log("This is the req body from :id--->", req.body)
         // save the workout to variable called work
-        // console.log("This is the PARAMS ID", req.params.id)
-        // console.log("This is the req.body", req.body)
-        // const workout = req.body
-        // console.log(workout)
-        // db.workout.updateOne(workout)
+        console.log("This is the PARAMS ID", req.params.id)
+        
     });
 
     app.post("/api/workouts", function (req, res) {
